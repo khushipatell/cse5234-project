486edfc6e6ba8f25dae4fa3a4be4cf97e7f4ae7e
skansher <shergill.02.muskan@gmail.com>
2024-10-06 18:52:02 -0400

    Update Header

--- collapsing-drums/src/components/header.jsx
+++ collapsing-drums/src/components/header.jsx
@@ -11,13 +11,51 @@ const Header = () => {
         navigate("/shoppingcart");
     };
 
+    const handleHomeClick = () => {
+        navigate("/home");
+    };
+    
+    const handleProductClick = () => {
+        navigate("/purchase");
+    };
+
+    const handleAboutClick = () => {
+    };
+
+    const handleContactClick = () => {
+    };
+
     return (
-        <div className="header">
-            <h1 className="header-title">Collapsing Drums</h1>
-            <FaShoppingCart
-                className="shopping-cart"
-                onClick={handleCartClick}
+        <div className="header row">
+
+            {/* Logo */}
+            <div className="header-logo col-md-4 align-self-start" onClick={handleHomeClick}>
+                <img className="logo" src="/img/logo.png" alt="Logo"/>
+                <h1 className="header-title">Collapsing Drums</h1>
+            </div>
+
+            {/* About Page */}
+            <div className="page col-md-2 align-self-end" onClick={handleAboutClick}>
+                <h3>About Us</h3>    
+            </div>
+
+            {/* Product Page */}
+            <div className="page col-md-2 align-self-end" onClick={handleProductClick}>
+                <h3>Products</h3>    
+            </div>
+
+            {/* Contact Page */}
+            <div className="page col-md-2 align-self-end" onClick={handleContactClick}>
+                <h3>Contact</h3>    
+            </div>
+
+            {/* Shopping Cart */}
+            <div className="col-md-2 align-self-center">
+                <FaShoppingCart
+                    className="shopping-cart"
+                    onClick={handleCartClick}
             />
+            </div>
         </div>
     );
 };
---
+++ collapsing-drums/src/components/home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./header";
+//import "../css/home.css";
+import "../css/site.css";
+
+const Home = () => {
+
+    return (
+        <div className="body container-fluid">
+            <Header />
+
+        </div>
+
+    );
+};
+
+export default Home;
